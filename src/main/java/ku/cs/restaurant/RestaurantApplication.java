57510ae4b64f33f5529c92440550cb5636c62f2c
kornavit <kornavit.p@ku.th>
2023-08-24 13:02:01 +0700

    first commit

---
+++ src/main/java/ku/cs/restaurant/RestaurantApplication.java
@@ -0,0 +1,14 @@
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		// ใช้ในการ run ตัวของ spring-boot
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
